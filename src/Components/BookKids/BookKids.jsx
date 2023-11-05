30cb4faf46355021231616089a57b4b938785dd6
mohima13 <modakmohima46@gmail.com>
2023-11-06 00:03:31 +0600

    install metarial ui for dynamic rating

--- src/Components/BookKids/BookKids.jsx
+++ src/Components/BookKids/BookKids.jsx
@@ -5,22 +5,25 @@ const BookKids = () => {
   const bookKidsLoader = useLoaderData();
   console.log(bookKidsLoader);
   return (
-    <div>
+    <div className="flex justify-center items-center">
       <h1>{bookKidsLoader.length}</h1>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 gap-5 m-16">
         {bookKidsLoader.map((kids) => (
           <div className="border">
-            <div>
-              <img src={kids.photo} alt="" />
+            <div className="flex justify-center items-center">
+              <img src={kids.photo} className="" />
             </div>
 
-            <div>
-              {kids.name}
-              {kids.quantity}
-              {kids.author}
-              {kids.quantity}
-              {kids.description}
-              {kids.rating}
+            <div className="text-center">
+              <h1>{kids.name}</h1>
+              <h1>Quantity: {kids.quantity}</h1>
+              <h1>category: {kids.category}</h1>
+              <h1>Author Name: {kids.author}</h1>
+              <div>
+                <h1>{kids.rating}</h1>
+                <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+              </div>
+              <h1>{kids.description}</h1>
             </div>
           </div>
         ))}
